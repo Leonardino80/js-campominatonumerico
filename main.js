49bab6861553ebc381f99c85554027147502ef11
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-03 04:16:20 +0200

    consegna finale

--- main.js
+++ main.js
@@ -20,26 +20,35 @@ while (numeri_random.length < 16) {
   numeri_random.push(numero);
   }
 }
+
 console.log(numeri_random)
 
 // In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta,
-var numero_presente = false;
-var punteggio = [];
+var punteggio = 0;
+var numeri_inseriti =[]
 // ciclo do while di richiesta
+var partita_in_corso = true;
 var i = 0;
-while (i < 84) {
-  var numero_utente = parseInt(prompt('inserisci un numero'));
-  if(numeri_random[i] === numero_utente){
-    numero_presente = true;
-  }
-  if( numero_presente) {
-    alert('hai perso');
-    break;
+while (partita_in_corso) {
+  var numero_utente = parseInt(prompt('inserisci un numero da 1 a 100'));
+  if(numeri_random.includes(numero_utente)){
+// se il numero è presente nella lista dei numeri generati, la partita termina,
+    partita_in_corso = false;
+    console.log('hai perso');
+    numeri_inseriti.push(numero_utente);
+  // altrimenti continua chiedendo all’utente un altro numero.
+  } else if ((numeri_inseriti.includes(numero_utente)) || (numero_utente === 0) || (isNaN(numero_utente)) || (parseInt(numero_utente) > 100)) {
+    alert('numero già inserito o non valido!');
+    console.log('Questi sono i numeri tra 1 e 100 che hai inserito : ' + numeri_inseriti);
+  } else if (partita_in_corso) {
+    numeri_inseriti.push(numero_utente);
+    punteggio += 1;
+    console.log(punteggio + 'punti')
+  } else {
+    console.log('Hai vinto! il tuo punteggio è ' + punteggio + ' punti')
   }
   i++;
 }
-
-
-  // se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+alert ('Hai perso! il tuo punteggio finale è   ' + punteggio + ' punti');
+console.log('Questi sono i numeri tra 1 e 100 che hai inserito : ' + numeri_inseriti);
